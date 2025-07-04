085d25599d674bae61d14b23a978f20588945ed4
= <soliemansnossy@gmail.com>
2025-07-04 20:36:01 +0200

    `Added favicon link to head section`

--- partials/head.php
+++ partials/head.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>أكاديمية رِواء</title>
+  <link rel="icon" href="/al mansa/Public/assets/images/logo2.png" type="image/x-icon">
   <link rel="stylesheet" href="/al mansa/Public/assets/css/bootstrap.rtl.min.css">
   <link rel="stylesheet" href="/al mansa/Public/assets/css/style.css">
   <link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
